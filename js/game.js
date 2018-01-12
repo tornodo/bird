52fbd7656b9b3744b1b5bab98da9afb922fbf603
海盗 <tornodo@vip.qq.com>
2018-01-12 18:00:54 +0800

    游戏源码

--- js/game.js
+++ js/game.js
@@ -25,6 +25,7 @@ var night = '#0493a2';
 var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, 'game');
 
 game.states = {};
+// 引导
 game.states.boot = function() {
     this.preload = function() {
         this.load.image('loading', 'assets/image/progress.png');
@@ -33,7 +34,7 @@ game.states.boot = function() {
         this.state.start('preloader');
     }
 }
-
+// 用来显示资源加载进度
 game.states.preloader = function() {
     this.preload = function() {
         var loadingSprite = this.add.sprite((this.world.width - 311) / 2, this.world.height / 2, 'loading');
@@ -49,32 +50,41 @@ game.states.preloader = function() {
 }
 //var chimney;
 //function test() {
-//	chimney = game.add.sprite(300, 300, 'ui', '6.png');
-//	chimney.type = "down";
-//	chimney.anchor.set(0.5);
-//	game.physics.arcade.enable(chimney);
-//	var halfH = chimney.height / 2;
-//	var body = game.add.sprite(0, 0, 'ui', '9.png');
-//	body.anchor.set(0.5, -0.5);
-//	game.physics.arcade.enable(body);
+//	chimney = game.add.sprite(100, 300, 'ui', '6.png');
+//	chimney.anchor.set(0.5);//烟囱头的锚点设为中心点
+//	chimney.angle = 180;//角度转动180°
+//	game.physics.arcade.enable(chimney);//对烟囱头启动物理引擎
+//	var halfH = chimney.height / 2;//烟囱头高度的一半
+//	var body = game.add.sprite(0, 0, 'ui', '9.png');//烟囱体
+//	body.anchor.set(0.5, 1.5);//同样设置锚点，相对与烟囱头的锚点
+//	body.angle = 180;//角度转动180°
+//	game.physics.arcade.enable(body);//启动物理引擎
 //	chimney.addChild(body);
-//	var n = Math.floor((chimney.y - halfH) / body.height);
+//	var n = Math.floor((game.height - halfH) / body.height);//计算还需要多少烟囱体
 //	for (var j = 0;j < n;j++) {
 //		var b = game.add.sprite(0, 0, 'ui', '9.png');
-//		b.anchor.set(0.5, -0.5 - (j + 1));
+//		b.angle = 180;//角度转动180°
+//		b.anchor.set(0.5, 1.5 + (j + 1));
 //		chimney.addChild(b);
 //		game.physics.arcade.enable(b);
 //	}
 //}
+// 游戏菜单
 game.states.menu = function() {
+	this.preload = function() {
+//      game.physics.startSystem(Phaser.Physics.ARCADE);
+	},
     this.create = function() {
         this.bg = this.add.sprite(0, 0, 'bg');
         this.bird = this.add.sprite(0, 0, 'ui', 'bird1.png');
+//      game.physics.arcade.enable(this.bird);
         this.bird.anchor.set(0.5);
         this.bird.x = game.world.centerX;
         this.bird.y = game.world.centerY - 100;
         this.bird.animations.add('bird_fly', ['bird1.png', 'bird2.png', 'bird3.png'], 20, true, false);
         this.bird.animations.play('bird_fly');
+//      this.bird.body.gravity.y = birdDropSpeed;
+//      game.input.onDown.add(this.fly, this); //给鼠标按下事件绑定鸟的飞翔动作
         this.startButton = this.add.sprite(0, 0, 'ui', 'button.png');
 		this.startButton.anchor.set(0.5);
 		this.startButton.x = game.world.centerX;
@@ -82,7 +92,17 @@ game.states.menu = function() {
 		this.startButton.inputEnabled = true;
 		this.startButton.input.useHandCursor = true;
 		this.startButton.events.onInputDown.add(this.startGame, this);
+//		test();
     },
+    /*this.update = function() {
+    	if(this.bird.angle < 90) {
+    		this.bird.angle += 2.5;//一直朝下旋转鸟头
+    	}
+    },
+    this.fly = function() {
+		this.bird.body.velocity.y = -350; //给鸟设一个向上的速度
+    	game.add.tween(this.bird).to({angle:-30}, 100, null, true, 0, 0, false); //上升时头朝上的动画
+    },*/
     this.startGame = function() {
     	this.bg.destroy();
     	this.bird.destroy();
@@ -96,18 +116,20 @@ game.states.menu = function() {
     	});
     }*/
 }
-
+//游戏界面
 game.states.start = function() {
     this.preload = function() {
-        game.stage.backgroundColor = day;
+        game.stage.backgroundColor = day;//设置背景色为白天
         this.cloud1 = this.add.tileSprite(0, game.height - 68 * 2, game.width, 95, 'ui', 'cloud1.png');
-        this.cloud1.autoScroll(speed / 10, 0);
+        this.cloud1.autoScroll(speed / 10, 0);//自动重复滚动
         this.cloud2 = this.add.tileSprite(0, game.height - 68 * 2, game.width, 95, 'ui', 'cloud2.png');
-        this.cloud2.visible = false;
+        this.cloud2.autoScroll(speed / 10, 0);//自动重复滚动
+        this.cloud2.visible = false;//隐藏
         this.building1 = this.add.tileSprite(0, game.height - 68, game.width, 68, 'ui', 'building1.png');
-        this.building1.autoScroll(speed / 5, 0);
+        this.building1.autoScroll(speed / 5, 0);//自动重复滚动
         this.building2 = this.add.tileSprite(0, game.height - 68, game.width, 68, 'ui', 'building2.png');
-        this.building2.visible = false;
+        this.building2.autoScroll(speed / 5, 0);//自动重复滚动
+        this.building2.visible = false;//隐藏
         this.bang = this.add.audio('die');
         this.music = this.add.audio('music');
         this.input.maxPointers = 1;
@@ -123,6 +145,8 @@ game.states.start = function() {
         this.bird.anchor.set(0.5);
         this.bird.x = game.world.centerX;
         this.bird.y = game.world.centerY;
+        this.bird.animations.add('bird_fly', ['bird1.png', 'bird2.png', 'bird3.png'], 20, true, false);
+        this.bird.animations.play('bird_fly');
         this.bird.body.gravity.y = birdDropSpeed;
     	ChimneyGroupsUp = game.add.group();//上面的烟囱组
     	ChimneyGroupsDown = game.add.group();
@@ -136,14 +160,14 @@ game.states.start = function() {
         updateScore();
         moveAChimney();
         ChimneyGroupsUp.forEach(function(Chimney) {
-        	game.physics.arcade.collide(this.bird, Chimney);
-        	Chimney.children.forEach(function(item) {
+        	game.physics.arcade.collide(this.bird, Chimney);//烟囱头
+        	Chimney.children.forEach(function(item) {//烟囱体
         		game.physics.arcade.collide(this.bird, item);
         	}, this);
         }, this);
         ChimneyGroupsDown.forEach(function(Chimney) {
-        	game.physics.arcade.collide(this.bird, Chimney);
-        	Chimney.children.forEach(function(item) {
+        	game.physics.arcade.collide(this.bird, Chimney);//烟囱头
+        	Chimney.children.forEach(function(item) {//烟囱体
         		game.physics.arcade.collide(this.bird, item);
         	}, this);
         }, this);
@@ -153,18 +177,18 @@ game.states.start = function() {
     this.update = function() {
     	if(this.bird.x <= 0 || this.bird.x >= game.width || this.bird.y <= 0 || this.bird.y >= game.height) {
     		this.gameOver();
-    	}
+    	}//小鸟飞出或是跌落出舞台
         ChimneyGroupsUp.forEach(function(Chimney) {
         	if (Chimney.x + Chimney.width / 2 < this.bird.x + this.bird.width / 2 && Chimney.scored === false) {
         		Chimney.scored = true;
         		score += 1;
         		changeBGScore += 1;
         		updateScore();
-        	}
+        	}//如果小鸟飞过了烟囱，加一分
         	game.physics.arcade.overlap(this.bird, Chimney, this.gameOver, null, this);
         	Chimney.children.forEach(function(item) {
         		game.physics.arcade.overlap(this.bird, item, this.gameOver, null, this);
-        	}, this);
+        	}, this);//如果小鸟跟烟囱或是烟囱体有碰撞直接结束游戏
         }, this);
         ChimneyGroupsDown.forEach(function(Chimney) {
         	game.physics.arcade.overlap(this.bird, Chimney, this.gameOver, null, this);
@@ -177,13 +201,13 @@ game.states.start = function() {
     	}
     	if (changeBGScore === changeScore) {//看看是否该切换背景了
 			changeBGScore = 0;//清空统计
-			if(this.cloud2.visible === false) {
+			if(this.cloud2.visible === false) {//切换到黑夜
 				this.cloud2.visible = true;
 				this.building2.visible = true;
 				this.cloud1.visible = false;
 				this.building1.visible = false;
 				game.stage.backgroundColor = night;
-			} else {
+			} else {//切换到白天
 				this.cloud2.visible = false;
 				this.building2.visible = false;
 				this.cloud1.visible = true;
@@ -212,7 +236,7 @@ game.states.start = function() {
 	    game.state.start('stop');
     }
 }
-
+// 游戏结束界面
 game.states.stop = function() {
     this.preload = function() {
         game.stage.backgroundColor = day;
@@ -293,19 +317,19 @@ function initUpChimney() {
 		var chimney = game.add.sprite(game.width, game.height, 'ui', '6.png');
 		chimney.scored = false;
 		chimney.anchor.set(0.5);
-		chimney.angle = 180;
+		chimney.angle = 180;//角度转动180°
 		game.physics.arcade.enable(chimney);
 		var halfH = chimney.height / 2;
 		var body = game.add.sprite(0, 0, 'ui', '9.png');
 		body.anchor.set(0.5, 1.5);
-		body.angle = 180;
+		body.angle = 180;//角度转动180°
 		game.physics.arcade.enable(body);
 		chimney.addChild(body);
-		var n = Math.floor((chimney.y - halfH) / body.height);
+		var n = Math.floor((game.height - halfH) / body.height);
 		for (var j = 0;j < n;j++) {
 			var b = game.add.sprite(0, 0, 'ui', '9.png');
 			b.anchor.set(0.5, 1.5 + (j + 1));
-			b.angle = 180;
+			b.angle = 180;//角度转动180°
 			chimney.addChild(b);
 			game.physics.arcade.enable(b);
 		}
@@ -316,15 +340,15 @@ function initUpChimney() {
 }
 function initDownChimney() {
 	for(var i = 0;i < MaxChinmeyNum;i++) {
-		var chimney = game.add.sprite(game.width, game.height, 'ui', '6.png');
-		chimney.anchor.set(0.5);
-		game.physics.arcade.enable(chimney);
-		var halfH = chimney.height / 2;
-		var body = game.add.sprite(0, 0, 'ui', '9.png');
-		body.anchor.set(0.5, -0.5);
-		game.physics.arcade.enable(body);
+		var chimney = game.add.sprite(game.width, 0, 'ui', '6.png');
+		chimney.anchor.set(0.5);//烟囱头的锚点设为中心点
+		game.physics.arcade.enable(chimney);//对烟囱头启动物理引擎
+		var halfH = chimney.height / 2;//烟囱头高度的一半
+		var body = game.add.sprite(0, 0, 'ui', '9.png');//烟囱体
+		body.anchor.set(0.5, -0.5);//同样设置锚点，相对与烟囱头的锚点
+		game.physics.arcade.enable(body);//启动物理引擎
 		chimney.addChild(body);
-		var n = Math.floor((chimney.y - halfH) / body.height);
+		var n = Math.floor((game.height - halfH) / body.height);//计算还需要多少烟囱体
 		for (var j = 0;j < n;j++) {
 			var b = game.add.sprite(0, 0, 'ui', '9.png');
 			b.anchor.set(0.5, -0.5 - (j + 1));
